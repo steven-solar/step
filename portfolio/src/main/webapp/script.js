4f0b73507e1e4bfd8ca83ea5968c70328b712f04
Steven <steven.j.solar@gmail.com>
2020-05-29 16:20:41 -0400

    style comments to look like messages

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -19,10 +19,44 @@ function getComments() {
     fetch("/data").then(response => response.json()).then((comments) => {
       const commElem = document.getElementById('comments-container');
       commElem.innerHtml = '';
-      comments.forEach(c => {
-          var li = document.createElement("li");
-          li.innerText = c.name + ": " + c.text;
-          commElem.appendChild(li);
+       comments.forEach(c => {
+          console.log(c.text);
+          var d = document.createElement("div");
+          d.innerHTML = "<div class='comment-text'>" + c.text + "</div>";
+          d.classList.add("box");
+          commElem.appendChild(d);
+          var d2 = document.createElement("div");
+          d2.innerHTML = "<span class='comment-name'>" + c.name + "</span>" + 
+                        "<span class='comment-time'>" + ", " + getTimeStamp(c) + "</span>";
+          d2.classList.add("comment-info");
+          commElem.appendChild(d2);
       });
     });  
+}
+
+function getTimeStamp(comment) {
+  var time = ((new Date()).getTime() - comment.timestamp)/1000; 
+  var seconds = (time).toFixed(0);
+  var minutes = (time/60).toFixed(0);
+  var hours = (time/(60*60)).toFixed(0);
+  var days = (time/(60*60*24)).toFixed(0);
+  var years = (time/(60*60*24*365)).toFixed(0);
+  if (seconds < 1) {
+    return "just now";
+  }
+  if (seconds < 60) {
+    return seconds === 1 ? seconds + " second ago" : seconds + " seconds ago";
+  }
+  else if (minutes < 60) {
+    return minutes === 1 ? minutes + " second ago" : minutes + " minutes ago";
+  }
+  else if (hours < 24) {
+    return hours === 1 ? hours + " second ago" : hours + " hours ago";
+  }
+  else if (days < 365) {
+    return days === 1 ? days + " second ago" : days + " days ago";
+  }
+  else {
+    return years === 1 ? years + " second ago" : years + " years ago";
+  }
 }

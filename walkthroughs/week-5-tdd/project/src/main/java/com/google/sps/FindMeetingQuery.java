30af9ae1daba537ac724884cbdfd09f5e741bcf5
Steven <steven.j.solar@gmail.com>
2020-06-08 21:43:14 +0000

    implements query function, passes all tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,65 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 
 public final class FindMeetingQuery {
+
+  public Collection<TimeRange> mergeTimes(Collection<Event> events, MeetingRequest request) {
+    List<String> requestAttendees = new ArrayList(request.getAttendees());
+    List<TimeRange> mergedTimes = new ArrayList<TimeRange>();
+    ArrayList<Event> eventList = new ArrayList(events);
+
+    if (eventList.size() == 0) {
+      return mergedTimes;
+    }
+
+    Collections.sort(eventList, Event.ORDER_BY_START);
+
+    TimeRange last = TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0);
+    for (Event e : events) {
+      if (!Collections.disjoint(requestAttendees, e.getAttendees())) {
+          if (mergedTimes.size() == 0) {
+            last = e.getWhen();
+            mergedTimes.add(last);
+          }
+          
+        if (last.overlaps(e.getWhen())) {
+          last = TimeRange.fromStartEnd(last.start(), Math.max(last.end(), e.getWhen().end()), false);
+          mergedTimes.set(mergedTimes.size() - 1, last);
+        }
+        else {
+          last = e.getWhen();
+          mergedTimes.add(last);
+        }
+      }
+    }
+    return mergedTimes;
+  }
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Collection<TimeRange> mergedTimes = mergeTimes(events, request);
+    Collection<TimeRange> availableTimes = new ArrayList<>();
+    int start = TimeRange.START_OF_DAY;
+    int end = TimeRange.START_OF_DAY;
+    for (TimeRange t : mergedTimes) {
+      end = t.start();
+      if (end - start >= request.getDuration()) {
+        availableTimes.add(TimeRange.fromStartEnd(start, end, false));
+      }
+      start = t.end();
+    }
+    end = TimeRange.END_OF_DAY;
+    if (end - start >= request.getDuration()) {
+      availableTimes.add(TimeRange.fromStartEnd(start, end, true));
+    }
+    return availableTimes;
   }
 }
